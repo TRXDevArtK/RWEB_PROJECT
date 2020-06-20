b6a5bea6ac481ad0a8a3181011bf86bd68335c7a
TRXDevArtK <trxdev@gmx.com>
2020-06-20 18:01:51 +0700

    20-jun-20 Update :
    -Add database baru (artikel,agenda)

---
+++ db/20-Jun-20/rweb.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 20, 2020 at 01:00 PM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rweb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `agenda`
+--
+
+CREATE TABLE `agenda` (
+  `id` int(5) NOT NULL,
+  `judul` varchar(25) NOT NULL,
+  `deskripsi` varchar(35) NOT NULL,
+  `mulai` date NOT NULL,
+  `berakhir` date NOT NULL,
+  `file` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `artikel`
+--
+
+CREATE TABLE `artikel` (
+  `id` int(5) NOT NULL,
+  `judul` varchar(25) NOT NULL,
+  `gambar` longblob NOT NULL,
+  `tanggal` date NOT NULL,
+  `penulis` varchar(35) NOT NULL,
+  `html` longblob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelanggan`
+--
+
+CREATE TABLE `pelanggan` (
+  `ktp` bigint(20) NOT NULL,
+  `id` int(11) NOT NULL,
+  `nama` varchar(75) NOT NULL,
+  `jk` char(1) NOT NULL,
+  `tgl` date NOT NULL,
+  `tlp` bigint(12) NOT NULL,
+  `email` varchar(254) NOT NULL,
+  `verifikasi` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pelanggan`
+--
+
+INSERT INTO `pelanggan` (`ktp`, `id`, `nama`, `jk`, `tgl`, `tlp`, `email`, `verifikasi`) VALUES
+(112244, 23523, 'aku adalah6666666a', 'L', '1999-02-01', 12314242, 'asd', 1),
+(112255, 1122334455, 'Aji Gelar', 'L', '1999-08-07', 85268043433, 'refaldi1700018013@webmail.uad.ac.ie', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `agenda`
+--
+ALTER TABLE `agenda`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `artikel`
+--
+ALTER TABLE `artikel`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`ktp`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
